582181bb9c418ffc1d3577a5c19bffcefb7d547d
ktcl <ktcl444@gmail.com>
2019-12-04 08:25:14 +0800

    my-pake first extension

---
+++ my-pake/src/MyPake.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace My\Pake;
+/**
+ * Created by PhpStorm.
+ * User: gaoxuxu
+ * Date: 2018/5/3
+ * Time: 16:31
+ */
+class MyPake
+{
+	public function say()
+	{
+		return 'my first extension';
+	}
+}
